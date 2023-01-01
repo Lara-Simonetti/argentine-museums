0e6afab0629a10fe7c274c974b7bd8a80a62a0b6
Lara <the.lemonetti@gmail.com>
2022-12-31 22:39:05 -0800

    Changed display of museums, added navigation

--- src/app/museum/museum-list/museum-list.component.ts
+++ src/app/museum/museum-list/museum-list.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { waitForAsync } from '@angular/core/testing';
 import { Museum } from '../museum';
 import { MuseumService } from '../museum.service';
-import { pluck} from 'rxjs/operators';
 import { ApiResults } from '../apiResults';
 
 @Component({
@@ -12,8 +11,9 @@ import { ApiResults } from '../apiResults';
 })
 export class MuseumListComponent implements OnInit {
 
-  museums: Array<Museum> = [];
   apiResults: ApiResults = {} as ApiResults;
+  museums: Array<Museum> = [];
+  currentMuseum: Museum = {} as Museum;
 
   constructor(private museumService: MuseumService) { }
 
@@ -21,18 +21,64 @@ export class MuseumListComponent implements OnInit {
     this.museumService.getMuseums().subscribe((apiResults) => {
       this.apiResults = apiResults;
       this.museums = this.apiResults.results;
+      this.currentMuseum = this.museums[0];
     });
   }
 
   ngOnInit() {
     this.getMuseums();
+    this.currentMuseum = this.museums[0];
+    console.log(this.currentMuseum);
   }
 
+//Checks for empty values so that their lines are not displayed
   checkEmptyValue(value: string) {
     if(value===""){
       return false
     }
     else{
+      return true
+    }
+  }
+
+//Navigation functions
+  isLastMuseum(museum:Museum){
+    if(museum === this.museums[-1]){
+      return true;
+    }
+    else{
+      return false
+    }
+  }
+
+  getMuseumIndex(museum: Museum){
+    var index: number = 0;
+    for(let i = 0; i < this.museums.length; i++){
+      if(museum.id == this.museums[i].id){
+        index = i;
+      }
+    }
+    return index;
+  }
+
+  getNextMuseum(museum:Museum){
+    var currentMuseumIndex = this.getMuseumIndex(museum);
+    if(this.isLastMuseum(museum)){
+      return false
+    }
+    else{
+      this.currentMuseum = this.museums[currentMuseumIndex + 1]
+      return true
+    }
+  }
+
+  getPreviousMuseum(museum:Museum){
+    var currentMuseumIndex = this.getMuseumIndex(museum);
+    if(currentMuseumIndex == 0){
+      return false
+    }
+   else{
+      this.currentMuseum = this.museums[currentMuseumIndex - 1]
       return true
     }
   }
