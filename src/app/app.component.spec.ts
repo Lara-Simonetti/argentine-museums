6417806cc969325a79bb9ffaf978aa241f90a4d5
Lara <the.lemonetti@gmail.com>
2023-01-21 23:05:59 -0800

    Added base for testing file

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('argentine-museums');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('argentine-museums app is running!');
-  });
 });
--- src/app/museum/museum-list/museum-list.component.spec.ts
+++ src/app/museum/museum-list/museum-list.component.spec.ts
@@ -2,15 +2,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
+import { faker } from '@faker-js/faker';
 import { MuseumListComponent } from './museum-list.component';
+import { Museum } from '../museum';
 
 describe('MuseumListComponent', () => {
   let component: MuseumListComponent;
   let fixture: ComponentFixture<MuseumListComponent>;
+  let debug: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientModule],
       declarations: [ MuseumListComponent ]
     })
     .compileComponents();
@@ -19,10 +23,34 @@ describe('MuseumListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(MuseumListComponent);
     component = fixture.componentInstance;
+
+    //Creates randomized museum information
+    faker.seed(123);
+
+    for(let i = 0; i < 10; i++) {
+      const museum = new Museum(
+        parseInt(faker.datatype.uuid()),
+        faker.internet.url(),
+        faker.internet.url(),
+        faker.company.bs(),
+        faker.address.streetAddress(),
+        faker.phone.imei(),
+        faker.lorem.paragraphs(3),
+        faker.internet.email(),
+        faker.address.city(),
+        faker.address.city(),
+        null
+      );
+      component.museums.push(museum);
+    }
+
     fixture.detectChanges();
+    debug = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+
 });
